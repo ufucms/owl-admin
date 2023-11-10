fcd22f6f06cf8ad000f2bc6db3c320542da83ddf
Eric <liushun@aliyun.com>
2023-11-10 21:59:53 +0800

    更新

--- src/Console/Module/InitCommand.php
+++ src/Console/Module/InitCommand.php
@@ -144,7 +144,7 @@ class InitCommand extends Command
 
     protected function getNamespace($name = null): string
     {
-        $prefix = AdminModule::isV10() ? 'App\\' : '';
+        $prefix = AdminModule::isV10() ? 'app\\' : '';
 
         return config('modules.namespace') . "\\{$this->module->getName()}\\{$prefix}{$name}";
     }
@@ -172,7 +172,7 @@ class InitCommand extends Command
     {
         $config   = $this->getPath("/config/admin.php");
         $contents = $this->getStub('config');
-        $_path    = 'Modules/' . $this->module->getName() . (AdminModule::isV10() ? '/App' : '') . '/bootstrap.php';
+        $_path    = 'Modules/' . $this->module->getName() . (AdminModule::isV10() ? '/app' : '') . '/bootstrap.php';
 
         $content = str_replace('{{bootstrap}}', 'base_path(\'' . $_path . '\')', $contents);
         $content = str_replace('{{route_prefix}}', $this->module->getLowerName() . '-api', $content);
